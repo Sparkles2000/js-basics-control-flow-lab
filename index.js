98f89e8714b746e25b038d35b3b0361c9139b6e2
Sparkles2000 <rucker.anna2000@gmail.com>
2021-04-01 02:45:47 -0500

    *updated

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if(feet<=400)
+    return `This one is on me!`
+      else if (400<feet && feet<2500)
+        return `I will gladly take your thirty bucks.`
+          else return `No can do.`
+
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  if (city===`NYC`)
+    return `Ok, sounds good.`
+      else return `No go.`
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case `generous`:
+      return `Thank you so much.`
+      break
+    case `not as generous`:
+      return `Thank you.`
+    default:
+     return `Bye.`
+  }
 }
+  // Write your code here!
